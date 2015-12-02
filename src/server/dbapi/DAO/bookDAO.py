b0925819ea9359b256f2ef4d59c446333a96848e
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2015-12-01 22:26:13 -0200

    Implements basic DAO interface

---
+++ src/server/dbapi/DAO/bookDAO.py
@@ -0,0 +1,25 @@
+import abc
+
+
+class BookDAO(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def get(self, id):
+        pass
+
+    @abc.abstractmethod
+    def get_all(self, limit=10, offset=0):
+        pass
+
+    @abc.abstractmethod
+    def insert(self, meal):
+        pass
+
+    @abc.abstractmethod
+    def update(self, meal):
+        pass
+
+    @abc.abstractmethod
+    def remove(self, id):
+        pass
---
+++ src/server/dbapi/DAO/sessionDAO.py
@@ -0,0 +1,21 @@
+import abc
+
+
+class SessionDAO(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def get(self, id):
+        pass
+
+    @abc.abstractmethod
+    def insert(self, meal):
+        pass
+
+    @abc.abstractmethod
+    def update(self, meal):
+        pass
+
+    @abc.abstractmethod
+    def remove(self, id):
+        pass
