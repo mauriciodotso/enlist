45ed87b59d3cfe2ac024160ba779a28872f8801a
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-01-05 03:55:03 -0200

    Adds new methods to userDAO

--- src/server/dbapi/DAO/userDAO.py
+++ src/server/dbapi/DAO/userDAO.py
@@ -11,3 +11,11 @@ class UserDAO(object):
     @abc.abstractmethod
     def insert_book(self, user_id, book_id):
         pass
+
+    @abc.abstractmethod
+    def get_movies(self, user_id):
+        pass
+
+    @abc.abstractmethod
+    def get_books(self, user_id):
+        pass
