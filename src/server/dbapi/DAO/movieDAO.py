954895df5dd598878c6c5c289d4db35300fd4e58
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-01-05 03:29:49 -0200

    Adds new method to movieDAO

--- src/server/dbapi/DAO/movieDAO.py
+++ src/server/dbapi/DAO/movieDAO.py
@@ -7,3 +7,7 @@ class MovieDAO(object):
     @abc.abstractmethod
     def get_all(self, limit=10, offset=0):
         pass
+
+    @abc.abstractmethod
+    def get_all_by_title(self, limit=10, offset=0):
+        pass
