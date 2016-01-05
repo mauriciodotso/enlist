c8db6ea59b8c147b4a1a0915a0eb019f8dd7c08e
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-01-05 03:55:24 -0200

    Implements userDAO methods with mongo

--- src/server/dbapi/mongo/userMongo.py
+++ src/server/dbapi/mongo/userMongo.py
@@ -7,7 +7,25 @@ class UserMongo(BaseMongo, UserDAO):
         super(UserMongo, self).__init__(database)
 
     def insert_movie(self, user_id, movie_id):
-        pass
+        try:
+            return self.table.update({'_id': user_id}, {'$addToSet': {'movies': movie_id}})
+        except Exception:
+            raise Exception
 
     def insert_book(self, user_id, book_id):
-        pass
+        try:
+            return self.table.update({'_id': user_id}, {'$addToSet': {'books': movie_id}})
+        except Exception:
+            raise Exception
+
+    def get_movies(self, user_id):
+        try:
+            return self.table.find({'_id': user_id}, {'movies':  1, '_id': 0}})
+        except Exception:
+            raise Exception
+
+    def get_books(self, user_id):
+        try:
+            return self.table.find({'_id': user_id}, {'books':  1, '_id': 0}})
+        except Exception:
+            raise Exception
