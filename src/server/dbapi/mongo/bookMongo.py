14bd6639e010536e0e435809a7dc9c15fef0e89d
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-01-10 06:08:47 -0200

    Bug fix

--- src/server/dbapi/mongo/bookMongo.py
+++ src/server/dbapi/mongo/bookMongo.py
@@ -3,14 +3,13 @@ from baseMongo import BaseMongo
 import pymongo
 
 
-
 class BookMongo(BaseMongo, BookDAO):
     def __init__(self, database):
             super(BookMongo, self).__init__(database, database.books)
 
     def get_all(self, limit=10, offset=0):
         try:
-            cursor = self.table.find().sort('title', pymongo.DESCENDING).limit(limit).skip(offset*limit)
+            cursor = self.table.find().sort('title', pymongo.ASCENDING).limit(limit).skip(offset*limit)
             total = self.table.find().count()
             result = []
 
@@ -23,12 +22,12 @@ class BookMongo(BaseMongo, BookDAO):
 
     def get_all_by_title(self, title, limit=10, offset=0):
         try:
-            cursor = self.table.find({'title': {'$regex': ('.*' + title + '.*')}}).sort('title', pymongo.DESCENDING).limit(limit).skip(offset*limit)
-            total = self.table.find().count()
+            cursor = self.table.find({'title': {'$regex': title}}).sort('title', pymongo.ASCENDING).limit(limit).skip(offset*limit)
+            total = self.table.find({'title': {'$regex': title}}).count()
             result = []
             
             for book in cursor:
-                result.append(movie)
+                result.append(book)
 
             return result, total
         except Exception:
@@ -36,8 +35,8 @@ class BookMongo(BaseMongo, BookDAO):
 
     def get_all_by_user(self, user_id, limit=10, offset=0):
         try:
-            books = self.database.users.find_one({'_id': user_id}, {'_id': 0, 'books': 1})
-            cursor = self.table.find({'_id': {'$in': books}}).sort('title', pymongo.DESCENDING).limit(limit).skip(offset*limit)
+            books = self.database.users.find_one({'_id': user_id}, {'_id': 0, 'books': 1})['books']
+            cursor = self.table.find({'_id': {'$in': books}}).sort('title', pymongo.ASCENDING).limit(limit).skip(offset*limit)
             total = len(books)
             result = []
 
