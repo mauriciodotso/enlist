2396046b244364be3709ae93b4a6d4bc78411987
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-01-14 02:25:57 -0200

    Updates test bash script

--- src/server/__tests__/run_tests.sh
+++ src/server/__tests__/run_tests.sh
@@ -6,4 +6,9 @@ echo "##################"
 echo ""
 python2 -m unittest discover -s "./dbapi" -p "*_test.py"
 
+echo "##################"
+echo "#Testing REST API#"
+echo "##################"
+echo ""
+python2 -m unittest discover -s "./rest" -p "*_test.py"
 
