ec74a180eed9a8a8a66bd71972b8ad39995d1c8e
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-01-23 03:45:58 -0200

    - Implements Book and Movie data representation.

---
+++ src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/Book.java
@@ -0,0 +1,16 @@
+package com.nkbits.apps.enlist;
+
+/**
+ * Created by nakayama on 1/23/16.
+ */
+public class Book {
+    public String title;
+    public int edition;
+    public int year;
+
+    public Book(String title, int edition, int year){
+        this.title = title;
+        this.edition = edition;
+        this.year = year;
+    }
+}
---
+++ src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/Movie.java
@@ -0,0 +1,14 @@
+package com.nkbits.apps.enlist;
+
+/**
+ * Created by nakayama on 1/23/16.
+ */
+public class Movie {
+    public String title;
+    public int year;
+
+    public Movie(String title, int year){
+        this.title = title;
+        this.year = year;
+    }
+}
