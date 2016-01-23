1882f9f1a06071b74c7a463532424082d0502b94
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-01-23 01:22:44 -0200

    Implement HTTP Request client

---
+++ src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/HTTPRequest.java
@@ -0,0 +1,24 @@
+package com.nkbits.apps.enlist;
+
+import com.loopj.android.http.*;
+
+/**
+ * Created by nakayama on 1/23/16.
+ */
+public class HTTPRequest {
+    private static final String BASE_URL = "http://192.168.25.58:5000";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
---
+++ src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/ListView.java
@@ -0,0 +1,7 @@
+package com.nkbits.apps.enlist;
+
+/**
+ * Created by nakayama on 1/22/16.
+ */
+public class ListView {
+}
