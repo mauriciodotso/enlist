3b975befec3eaad2d7029cfcb8d74d27feb2320e
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-01-25 05:54:46 -0200

    - Adds Book Item interface implementation

--- src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/Book.java
+++ src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/Book.java
@@ -1,9 +1,12 @@
 package com.nkbits.apps.enlist;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by nakayama on 1/23/16.
  */
-public class Book {
+public class Book implements Item{
     public String title;
     public int edition;
     public int year;
@@ -12,5 +15,23 @@ public class Book {
         this.title = title;
         this.edition = edition;
         this.year = year;
+    }
+
+    public Book(JSONObject json) {
+        try {
+            this.title = json.getString("title");
+            this.edition = json.getInt("edition");
+            this.year = json.getInt("year");
+        } catch (JSONException e) {
+            //ToDo: Handle exception
+        }
+    }
+
+    public JSONObject JSON(){
+        try {
+            return new JSONObject("{'title':" + this.title + ", 'year':" + this.year + ", 'edition':" + this.edition + "}");
+        }catch (JSONException e){
+            return null;
+        }
     }
 }
