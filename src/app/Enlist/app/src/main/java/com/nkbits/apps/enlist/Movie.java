502c2c8615d12a7276e328eac0bb0c147869257c
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-01-26 18:22:19 -0200

    - Adds Movie Item interface implementation

--- src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/Movie.java
+++ src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/Movie.java
@@ -1,14 +1,34 @@
 package com.nkbits.apps.enlist;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by nakayama on 1/23/16.
  */
-public class Movie {
+public class Movie implements Item{
     public String title;
     public int year;
 
     public Movie(String title, int year){
         this.title = title;
         this.year = year;
+    }
+
+    public Movie(JSONObject json) {
+        try {
+            this.title = json.getString("title");
+            this.year = json.getInt("year");
+        } catch (JSONException e) {
+            //ToDo: Handle exception
+        }
+    }
+
+    public JSONObject JSON(){
+        try {
+            return new JSONObject("{'title':" + this.title + ", 'year':" + this.year + "}");
+        }catch (JSONException e){
+            return null;
+        }
     }
 }
