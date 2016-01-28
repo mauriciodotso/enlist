4f65a98d8f4c3a7090b193ccb54b71e72eefa79d
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-01-27 22:17:54 -0200

    - Changes HTTPRequest to Synchronous.

--- src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/HTTPRequest.java
+++ src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/HTTPRequest.java
@@ -8,13 +8,13 @@ import com.loopj.android.http.*;
 public class HTTPRequest {
     private static final String BASE_URL = "http://192.168.25.58:5000";
 
-    private static AsyncHttpClient client = new AsyncHttpClient();
+    private static SyncHttpClient client = new SyncHttpClient();
 
-    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+    public static void get(String url, RequestParams params, JsonHttpResponseHandler responseHandler) {
         client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
-    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+    public static void post(String url, RequestParams params, JsonHttpResponseHandler responseHandler) {
         client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
