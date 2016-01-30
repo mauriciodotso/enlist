76e8aa9e1e85d5fb3565bc138366b82d028c6458
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-01-30 14:37:18 -0200

    - Starts android BookFacade tests implementation

---
+++ src/app/Enlist/app/src/androidTest/java/com/nkbits/apps/enlist/BookFacadeTest.java
@@ -0,0 +1,48 @@
+package com.nkbits.apps.enlist;
+
+import android.test.InstrumentationTestCase;
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.mockwebserver.MockResponse;
+import com.squareup.okhttp.mockwebserver.MockWebServer;
+import com.squareup.okhttp.mockwebserver.RecordedRequest;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class BookFacadeTest extends InstrumentationTestCase {
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testGet() throws Exception{
+        MockWebServer server = new MockWebServer();
+        MockResponse response = new MockResponse()
+                .addHeader("Content-Type", "application/json; charset=utf-8")
+                .addHeader("Cache-Control", "no-cache")
+                .setBody("{_id: '507f191e810c19729de860ea', title: 'title', edition: 1, year: 1991}");
+
+        server.enqueue(response);
+        server.start();
+
+        HttpUrl baseUrl = server.url("");
+
+        HTTPRequest.setBaseUrl(baseUrl.toString());
+        Book book = BookFacade.get("507f191e810c19729de860ea");
+
+        assertEquals("507f191e810c19729de860ea", book._id);
+        assertEquals("title", book.title);
+        assertEquals(1, book.edition);
+        assertEquals(1991, book.year);
+
+        RecordedRequest request = server.takeRequest();
+        assertEquals("/book/get", request.getPath());
+
+        server.shutdown();
+    }
+}
