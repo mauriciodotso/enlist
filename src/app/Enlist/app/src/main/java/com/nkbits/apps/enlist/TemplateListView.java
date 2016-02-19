1031f6a2421fa170df6b3867b63ffae6881467e9
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2016-02-18 22:08:50 -0200

    - Implements endless scrolling.

--- src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/TemplateListView.java
+++ src/app/Enlist/app/src/main/java/com/nkbits/apps/enlist/TemplateListView.java
@@ -1,10 +1,13 @@
 package com.nkbits.apps.enlist;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ListView;
 
 import java.lang.reflect.Array;
@@ -17,6 +20,8 @@ import java.util.List;
 public class TemplateListView<T> extends Fragment {
     private ListView listView;
     private List<T> data;
+    private ListViewAdapter<T> adapter;
+    private String type;
 
     public static final String DATA = "data";
     public static final String VIEW = "view";
@@ -24,11 +29,11 @@ public class TemplateListView<T> extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.list_view, container, false);
-        ListViewAdapter<T> adapter;
 
         data = Arrays.asList((T[]) getArguments().getSerializable(DATA));
+        type = getArguments().getString(VIEW);
 
-        switch (getArguments().getString(VIEW)){
+        switch (type){
             case "Book":
                 adapter = new ListViewAdapter<T>(getActivity(), R.layout.book_view, data);
                 break;
@@ -42,7 +47,85 @@ public class TemplateListView<T> extends Fragment {
 
         listView = (ListView)view.findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnScrollListener(new EndlessScrollListener(1));
 
         return view;
+    }
+
+    public class EndlessScrollListener implements AbsListView.OnScrollListener {
+        private int visibleThreshold = 5;
+        private int currentPage = 0;
+        private int previousTotal = 0;
+        private boolean loading = true;
+
+        public EndlessScrollListener() {
+        }
+        public EndlessScrollListener(int visibleThreshold) {
+            this.visibleThreshold = visibleThreshold;
+        }
+
+        @Override
+        public void onScroll(AbsListView view, int firstVisibleItem,
+                             int visibleItemCount, int totalItemCount) {
+            if (loading) {
+                if (totalItemCount > previousTotal) {
+                    loading = false;
+                    previousTotal = totalItemCount;
+                    currentPage++;
+                }
+            }
+
+            if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+                new SendRequest().execute(currentPage + 1);
+                loading = true;
+            }
+        }
+
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+        }
+    }
+
+    class SendRequest extends AsyncTask<Integer, Void, Boolean> {
+        ProgressDialog progressDialog = new ProgressDialog(getActivity());
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            progressDialog.setMessage("Loading...");
+            progressDialog.setIndeterminate(false);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressDialog.setCancelable(true);
+            progressDialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(Integer... option) {
+            T[] newData = null;
+
+            switch(type){
+                case "Book":
+                    newData = (T[])BookFacade.getAll(10, option[0]);
+                    break;
+                case "Movie":
+                    newData = (T[])MovieFacade.getAll(10, option[0]);
+                    break;
+            }
+
+            if(newData != null) {
+                for (T item : newData) {
+                    data.add(item);
+                }
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success){
+            super.onPostExecute(success);
+            progressDialog.dismiss();
+            adapter.notifyDataSetChanged();
+        }
     }
 }
